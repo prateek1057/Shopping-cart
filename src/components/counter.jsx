336a531b1eca93ae0f0cbfb6143129556db550ba
Prateek Sharma <61390535+prateek1057@users.noreply.github.com>
2022-06-09 21:16:33 +0530

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ render() {
                 <span><strong>+</strong></span>
             </button>
             <button onClick={()=>this.props.onDecrement(this.props.counterp)} className="btn btn-secondary btn-md m-2">
-                -
+                <span><strong>-</strong></span>
             </button>
             <button onClick={()=>this.props.onDelete(this.props.counterp.id)} className="btn btn-danger btn-sm m-2">Delete</button>
              
